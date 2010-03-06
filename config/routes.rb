a2c2d23882e7cbc0b80dad5a5745fb495139b194
Derek Croft <dc@blueru.by>
2010-03-06 10:48:15 -0500

    Clean up routes.rb for restful_authentication.  Add site_keys.rb to .gitignore.

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  # restful_authentication routes
   map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.login '/login', :controller => 'sessions', :action => 'new'
   map.register '/register', :controller => 'users', :action => 'create'
   map.signup '/signup', :controller => 'users', :action => 'new'
-  map.resources :users
 
+  map.resources :users
   map.resource :session
 
-  # RESTful routes
+  # Application resource routes
   map.resources :members
 
   # Named routes for the static site
