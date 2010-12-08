0a27d910c543e395fdf9208e05611149dfba9b5a
Markus Silpala <markus@silpala.com>
2010-12-08 00:42:05 -0600

    Split out views for profile view and edit. I feel like the current show and edit views ought to be layouts, so the _profile_details partials could be the views. To make this work, need to look up how to have the application layout yield a controller-specific layout.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,18 +2,16 @@ class ProfilesController < ApplicationController
   before_filter :load_profile
 
   def show
-    render "member"
   end
 
   def edit
-    render "member"
   end
 
   def update
     expertise_ids = params[:expertise].select { |k,v| v.has_key?(:has) }.collect { |e| e.first.to_i }
     @profile.expertise_ids = expertise_ids
-    if @profile.update_attributes(params[:profile]) 
-      redirect_to member_profile_path(@member) 
+    if @profile.update_attributes(params[:profile])
+      redirect_to member_profile_path(@member)
     else
       render :action => :edit
     end
@@ -22,7 +20,7 @@ class ProfilesController < ApplicationController
   protected
   def load_profile
     if params[:member_id]
-      @member = Member.find(params[:member_id]) 
+      @member = Member.find(params[:member_id])
       @profile = @member.profile
     elsif params[:id]
       @profile = Profile.find(params[:id])
